cc1c31f23e2ff46e0e819aa18999cd4ad8298fc8
João Pereira <joao.pereira@navenio.com>
2020-10-18 19:28:10 +0100

    Convert client code to typescript and running correctly

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-export default () => (
+export const App = () => (
   <div className="wrapper">
     <div className="menu-summary">
       <div className="container">
---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const outputDirectory = "lib/client";
+
+module.exports = {
+  entry: "./src/client/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader?limit=100000",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+    proxy: {
+      "/api": "http://localhost:8080",
+    },
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "lib/client"),
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+    }),
+  ],
+};
