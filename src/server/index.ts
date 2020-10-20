721092b187ce202637c91ff78a8a5cddc2440463
João Pereira <joao.pereira@navenio.com>
2020-10-20 20:43:39 +0100

    Add search ability to server

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,8 +6,17 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static("dist"));
 
-app.get("/api/items", (_: express.Request, res: express.Response) =>
-  res.send({ items })
-);
+app.get("/api/items", (req: express.Request, res: express.Response) => {
+  const search = req.query.search;
+  let result = items.slice();
+
+  if (search && typeof search === "string") {
+    const regex = new RegExp(`.*${search.toLowerCase()}.*`);
+    console.log(regex);
+    result = items.filter((item) => regex.test(item.name.toLowerCase()));
+  }
+
+  res.send({ items: result });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
