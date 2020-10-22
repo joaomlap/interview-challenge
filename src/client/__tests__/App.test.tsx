8f22b466748816cf9475361a8db9d4871279b40c
João Pereira <joao.pereira@navenio.com>
2020-10-22 18:03:06 +0100

    App and menu tests fixed

--- src/client/__tests__/App.test.tsx
+++ src/client/__tests__/App.test.tsx
@@ -1,11 +1,48 @@
+import fetch from "jest-fetch-mock";
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { App } from "../App";
+import { cache, SWRConfig } from "swr";
+
+fetch.enableMocks();
 
 // Example test
 describe("App tests", () => {
-  it("renders a message", () => {
-    render(<App />);
-    expect(screen.getByText("6 items")).toBeInTheDocument();
+  beforeEach(() => cache.clear());
+
+  it("inital state is correct", async () => {
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        items: [
+          {
+            id: 1001,
+            name:
+              "Kale Caesar Pasta, Turmeric Satay Broccoli & Lemon Cashew Greens",
+            dietaries: ["v", "ve", "df", "gf", "n!"],
+          },
+          {
+            id: 1002,
+            name: "Hake & Smoky Chickpeas, Brown Rice & Quinoa, Roasted Roots",
+            dietaries: ["gf", "df", "rsf"],
+          },
+        ],
+      })
+    );
+
+    const { container } = render(
+      <SWRConfig value={{ dedupingInterval: 0 }}>
+        <App />
+      </SWRConfig>
+    );
+
+    await waitFor(() => {
+      const menuItems = container.querySelectorAll(".clickable");
+      const menuPreviewItems = container.querySelectorAll(
+        ".menu-preview > .item"
+      );
+
+      expect(menuItems.length).toEqual(2);
+      expect(menuPreviewItems.length).toEqual(0);
+    });
   });
 });
--- src/client/__tests__/Menu.test.tsx
+++ src/client/__tests__/Menu.test.tsx
@@ -1,27 +1,37 @@
 import fetch from "jest-fetch-mock";
-import { act, findByText, render, waitFor } from "@testing-library/react";
+import { findByText, fireEvent, render, waitFor } from "@testing-library/react";
 import React from "react";
 import { Menu } from "../Menu";
+import { cleanup } from "@testing-library/react-hooks";
+import { cache, SWRConfig } from "swr";
 
 fetch.enableMocks();
 
 describe("Menu", () => {
   beforeEach(() => {
+    cleanup();
     fetch.resetMocks();
+    cache.clear();
   });
 
-  it("should render correctly when api sends empty array", () => {
+  it("should render correctly when api sends empty array", async () => {
     fetch.mockResponseOnce(
       JSON.stringify({
         items: [],
       })
     );
-    const { container } = render(<Menu addItem={() => {}} />);
+    const { container } = render(
+      <SWRConfig value={{ dedupingInterval: 0 }}>
+        <Menu addItem={() => {}} />
+      </SWRConfig>
+    );
 
-    waitFor(() => expect(container.querySelectorAll("li").length).toEqual(0));
+    await waitFor(() =>
+      expect(container.querySelectorAll("li").length).toEqual(0)
+    );
   });
 
-  it("should render correctly when api sends a populated array", () => {
+  it("should render correctly when api sends a populated array", async () => {
     fetch.mockResponseOnce(
       JSON.stringify({
         items: [
@@ -39,20 +49,63 @@ describe("Menu", () => {
         ],
       })
     );
-    const { container } = render(<Menu addItem={() => {}} />);
+    const { container } = render(
+      <SWRConfig value={{ dedupingInterval: 0 }}>
+        <Menu addItem={() => {}} />
+      </SWRConfig>
+    );
 
-    waitFor(() => {
+    await waitFor(() => {
       expect(container.querySelectorAll("li").length).toEqual(2);
     });
   });
 
   it("should render an error message when api errors", () => {
     fetch.mockReject();
-    const { container } = render(<Menu addItem={() => {}} />);
+    const { container } = render(
+      <SWRConfig value={{ dedupingInterval: 0 }}>
+        <Menu addItem={() => {}} />
+      </SWRConfig>
+    );
 
     waitFor(() => async () => {
       expect(container.querySelectorAll("li").length).toEqual(0);
       expect(await findByText(container, /An error occurred!/)).toBeTruthy();
+    });
+  });
+
+  it("should call addItem correctly", () => {
+    const mockFn = jest.fn();
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        items: [
+          {
+            id: 1001,
+            name:
+              "Kale Caesar Pasta, Turmeric Satay Broccoli & Lemon Cashew Greens",
+            dietaries: ["v", "ve", "df", "gf", "n!"],
+          },
+          {
+            id: 1002,
+            name: "Hake & Smoky Chickpeas, Brown Rice & Quinoa, Roasted Roots",
+            dietaries: ["gf", "df", "rsf"],
+          },
+        ],
+      })
+    );
+    const { container } = render(
+      <SWRConfig value={{ dedupingInterval: 0 }}>
+        <Menu addItem={mockFn} />
+      </SWRConfig>
+    );
+
+    waitFor(() => {
+      const items = container.querySelectorAll("li");
+      expect(items.length).toEqual(2);
+
+      items[0] && fireEvent.click(items[0]);
+
+      expect(mockFn).toHaveBeenCalledWith(items[0]);
     });
   });
 });
